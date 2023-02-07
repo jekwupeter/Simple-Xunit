6cd84f47404098c8070e47c22d4e691c3804acb5
jekwupeter <cephaspeter123@gmail.com>
2023-02-07 20:42:53 +0100

    Added testorderer to order test collection by display name

---
+++ Calculations/Calculations.Test/TestCollectionOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Calculations.Test
+{
+    public class TestCollectionOrder : ITestCollectionOrderer
+    {
+        public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+        {
+            return testCollections.OrderBy(c => c.DisplayName);
+        }
+    }
+}
