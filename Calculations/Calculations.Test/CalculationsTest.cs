bee53e0e76584ae4ef1043a2630e6aef28d88beb
jekwupeter <cephaspeter123@gmail.com>
2023-02-09 13:48:02 +0100

    Added test using member data

--- Calculations/Calculations.Test/CalculationsTest.cs
+++ Calculations/Calculations.Test/CalculationsTest.cs
@@ -70,6 +70,15 @@ namespace Calculations.Test
             Assert.Equal(expected, result);
         }
 
+        [MemberData(nameof(TestDataShare.IsOddOr_EvenData), MemberType = typeof(TestDataShare))]
+        [Theory]
+        public void CheckOddNumber_TestOddAndEvenUsingMemberData(int num, bool expected)
+        {
+            var Calc = _calculationsFixture.Calc;
+            var result = Calc.CheckOddNumber(num);
+            Assert.Equal(expected, result);
+        }
+
         public void Dispose()
         {
             stream.Close();
---
+++ Calculations/Calculations.Test/TestDataShare.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations.Test
+{
+    public static class TestDataShare
+    {
+        public static IEnumerable<object[]> IsOddOr_EvenData{
+            get{
+                yield return new object[] { 1, true };
+                yield return new object[] { 6, false };
+            }
+        }
+    }
+}
